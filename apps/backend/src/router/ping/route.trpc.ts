a7dfa6c0a3798554935dea30a7a32e869cc1460c
Sergei Dmitriev <iserdmi@gmail.com>
2025-08-21 07:05:30 +0400

    x

--- apps/backend/src/router/ping/route.trpc.ts
+++ apps/backend/src/router/ping/route.trpc.ts
@@ -2,7 +2,6 @@ import { BackendTrpc } from "@shmoject/backend/lib/trpc";
 
 export const pingTrpcRoute = BackendTrpc.baseProcedure().query(
   async ({ ctx }) => {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
     return {
       message: "pong",
     };
