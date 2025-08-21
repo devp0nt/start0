90a54f020260f6da47e688ec4b1500836113bff4
Sergei Dmitriev <iserdmi@gmail.com>
2025-08-21 05:34:13 +0400

    x

--- apps/site/app/root.tsx
+++ apps/site/app/root.tsx
@@ -9,6 +9,7 @@ import {
 import { GeneralLayout } from "@shmoject/site/components/GeneralLayout";
 
 import type { Route } from "./+types/root";
+import { TRPCProvider } from "@shmoject/site/lib/trpc.provider";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -42,7 +43,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <TRPCProvider>
+      <Outlet />
+    </TRPCProvider>
+  );
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
