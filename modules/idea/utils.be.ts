7d03d3c9288731915a774b3e2fdf1fb97a29b65c
Sergei Dmitriev <iserdmi@gmail.com>
2025-08-27 13:52:04 +0400

    x

--- modules/idea/utils.be.ts
+++ modules/idea/utils.be.ts
@@ -1,16 +1,1 @@
-export namespace IdeaBe {
-  export const ideas = [
-    {
-      id: 1,
-      title: "Idea 111",
-      description: "Description 1",
-    },
-    {
-      id: 2,
-      title: "Idea 2",
-      description: "Description 2",
-    },
-  ]
-
-  export type Idea = (typeof ideas)[number]
-}
+export namespace IdeaBe {}
