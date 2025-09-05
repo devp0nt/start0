13a3d607219df24d87b98e931c3708b8afed8cf9
Sergei Dmitriev <iserdmi@gmail.com>
2025-09-05 11:02:27 +0400

    x

--- modules/prisma/src/generated.be/prisma0/models.ts
+++
@@ -1,6 +0,0 @@
-export namespace Prisma0Models {
-  export const names = ["Idea", "AppConfigItem"];
-  export const idsKeys = ["ideaId", "appConfigItemId"];
-  export const namesWithCreatedAt = ["Idea"];
-  export const namesWithUpdatedAt = ["Idea"];
-}
