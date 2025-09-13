7323161f2244e282dbd8c29dd9420bb122f6a38a
Sergei Dmitriev <iserdmi@gmail.com>
2025-09-14 02:45:27 +0400

    x

--- tools/src/mono0/tsconfig.ts
+++ tools/src/mono0/tsconfig.ts
@@ -195,12 +195,12 @@ export class Mono0Tsconfig {
       file0: tsconfig.file0,
       unit: tsconfig.unit,
     })
-    const paths = Object.fromEntries(
-      units.map((unit) => [`${unit.name}/*`, [`${tsconfig.file0.fs0.toRel(unit.srcFs0.cwd)}/*`]]),
-    )
+    // const paths = Object.fromEntries(
+    //   units.map((unit) => [`${unit.name}/*`, [`${tsconfig.file0.fs0.toRel(unit.srcFs0.cwd)}/*`]]),
+    // )
     valueParsed.compilerOptions = {
       ...valueParsed.compilerOptions,
-      paths,
+      // paths,
     }
     await tsconfig.file0.write(JSON.stringify(valueParsed, null, 2), true)
   }
