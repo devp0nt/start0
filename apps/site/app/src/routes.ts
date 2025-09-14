23aec96d0a2a181755c590d2611877f14f3c3008
Sergei Dmitriev <iserdmi@gmail.com>
2025-09-14 23:18:24 +0400

    x

--- apps/site/app/src/routes.ts
+++ apps/site/app/src/routes.ts
@@ -2,13 +2,10 @@ import { index, layout, type RouteConfig, route } from "@react-router/dev/routes
 
 export default [
   layout("./components/GeneralLayout.tsx", [
+    index("./routes/generated/apps-site-app-src-pages-home-page-tsx.tsx"),
     route("ideas/:sn", "./routes/generated/modules-idea-site-src-view-page-tsx.tsx"),
     route("ideas", "./routes/generated/modules-idea-site-src-list-page-tsx.tsx"),
     route("*", "./routes/catchall.tsx", { id: "catchall1" })
   ]),
-  layout("../../core/src/components/GeneralLayout.tsx", [
-    index("./routes/generated/apps-site-app-src-pages-home-page-tsx.tsx"),
-    route("*", "./routes/catchall.tsx", { id: "catchall2" })
-  ]),
-  route("*", "./routes/catchall.tsx", { id: "catchall3" })
+  route("*", "./routes/catchall.tsx", { id: "catchall2" })
 ] satisfies RouteConfig
