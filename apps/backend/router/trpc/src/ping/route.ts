bb58a50dede77422b207b7e79ff310d11314b3b0
Sergei Dmitriev <iserdmi@gmail.com>
2025-09-21 01:10:45 +0400

    x

--- apps/backend/router/trpc/src/ping/route.ts
+++ apps/backend/router/trpc/src/ping/route.ts
@@ -3,7 +3,6 @@ import { BackendTrpc } from '@backend/core/lib/trpc'
 // TODO: combine all routes into one file and use generator to find desired routes
 
 export const pingTrpcRoute = BackendTrpc.baseProcedure().query(async () => {
-   f r
   return {
     message: 'pong',
   }
