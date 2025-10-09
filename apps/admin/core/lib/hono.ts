8d5cb30e07643073e647620215bd0c0d65f84a6a
Sergei Dmitriev <iserdmi@gmail.com>
2025-10-09 04:26:07 +0400

    x

--- apps/admin/core/lib/hono.ts
+++ apps/admin/core/lib/hono.ts
@@ -1,6 +1,6 @@
-import { createReactQueryHonoClient } from '@admin/core/lib/hono.rq'
 import type { HonoAdmin, HonoApp } from '@backend/hono-router'
 import { backendHonoAdminRoutesBasePath, backendHonoAppRoutesBasePath } from '@backend/shared/utils'
+import { createReactQueryHonoClient } from '@devp0nt/react-query-hono-client'
 
 export const honoAdminClient = createReactQueryHonoClient<HonoAdmin>(
   `${import.meta.env.VITE_BACKEND_URL}${backendHonoAdminRoutesBasePath}`,
