c0f281db62912ec18eecd1c952f7f65ab33768a4
Sergei Dmitriev <iserdmi@gmail.com>
2025-10-15 22:49:55 +0400

    x

--- .gen0rc.mjs
+++ .gen0rc.mjs
@@ -5,5 +5,5 @@ export default {
   // afterProcessCmd: (filePath) => `bun run lint ${filePath}`,
   debug: true,
   plugins: [getGen0PluginImportJs(), getGen0PluginMono0({ watcherEnabled: false })],
-  clientsGlob: ['./{apps,modules,tools}/**/*.{ts,tsx,js,jsx,mjs,json,jsonc}', 'justfile'],
+  clientsGlob: ['./{apps,modules,packages,tools}/**/*.{ts,tsx,js,jsx,mjs,json,jsonc}', 'justfile'],
 }
